d75a1f8b244e6b2314c5d785f8fb6428e3cac76c
Rabin Dongol <rawbean08@gmail.com>
2022-02-13 22:07:35 +0545

    changed main url

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,4 +1,4 @@
-export const MainUrl = 'http://localhost:8085/';
+export const MainUrl = 'https://sagun-baakas-admin.herokuapp.com/';
 
 export const configApiUrl = {
     USER_API: MainUrl + 'user',
