aa9118c525a5d56beb70e7444ee3041e219aec74
Rabin <rabin.dongol@btech.com.np>
2022-02-14 19:36:20 +0545

    added edit for product

--- src/app/pages/products/product-new/product-new.component.ts
+++ src/app/pages/products/product-new/product-new.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductModel} from '../model/product-model';
 import {ProductService} from '../service/product.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
 import {NgxImageCompressService} from 'ngx-image-compress';
 
@@ -13,23 +13,62 @@ import {NgxImageCompressService} from 'ngx-image-compress';
 export class ProductNewComponent implements OnInit {
 
     model: ProductModel = new ProductModel();
+    routeId;
 
     constructor(private service: ProductService,
-                private router: Router, private notify: ToastrService, private imageCompress: NgxImageCompressService) {
+                private router: Router, private notify: ToastrService,
+                private imageCompress: NgxImageCompressService, private route: ActivatedRoute) {
 
     }
 
     ngOnInit(): void {
+        this.route.params.subscribe(params => {
+            this.routeId = (params['id']);
+        });
+        this.service.display(true);
+        if (this.routeId) {
+            this.service.display(true);
+            this.service.getById(this.routeId)
+                .subscribe((data: any) => {
+                    this.model = data.data;
+
+                    this.service.display(false);
+                }, error1 => {
+                    this.service.display(false);
+                });
+        } else {
+            this.service.display(false);
+        }
     }
 
     onSubmit() {
-        this.service.save(this.model)
-            .subscribe((data: any) => {
-                this.notify.success(data.message, 'Success');
-                this.router.navigate(['/product/list']);
-            }, error => {
-                this.notify.error(error.error.message, 'Error');
-            });
+        this.service.display(true);
+        if (!this.routeId) {
+            this.service.save(this.model)
+                .subscribe((data: any) => {
+                    this.onSuccess(data);
+                }, error => {
+                    this.onError(error);
+                });
+        } else {
+            this.service.update(this.routeId, this.model)
+                .subscribe((data: any) => {
+                    this.onSuccess(data);
+                }, error => {
+                    this.onError(error);
+                });
+        }
+    }
+
+    onSuccess(data: any) {
+        this.service.display(false);
+        this.notify.success(data.message, 'Success');
+        this.router.navigate(['/product/list']);
+    }
+
+    onError(error) {
+        this.service.display(false);
+        this.notify.error(error.error.message, 'Error');
     }
 
 }
