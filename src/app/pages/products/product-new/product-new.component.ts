a8f7e7415fb8fceb06068f656a2b745bafbba3cc
Rabin <rabin.dongol@btech.com.np>
2022-02-14 14:41:50 +0545

    compressed image before upload

--- src/app/pages/products/product-new/product-new.component.ts
+++ src/app/pages/products/product-new/product-new.component.ts
@@ -1,49 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductModel } from '../model/product-model';
-import { ProductService } from '../service/product.service';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
+import {Component, OnInit} from '@angular/core';
+import {ProductModel} from '../model/product-model';
+import {ProductService} from '../service/product.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {NgxImageCompressService} from 'ngx-image-compress';
+
 @Component({
-  selector: 'app-product-new',
-  templateUrl: './product-new.component.html',
-  styleUrls: ['./product-new.component.sass']
+    selector: 'app-product-new',
+    templateUrl: './product-new.component.html',
+    styleUrls: ['./product-new.component.sass']
 })
 export class ProductNewComponent implements OnInit {
 
-  model: ProductModel = new ProductModel();
-  constructor(private service: ProductService, private router: Router, private notify: ToastrService) {
-  }
+    model: ProductModel = new ProductModel();
 
-  ngOnInit(): void {
-  }
+    constructor(private service: ProductService,
+                private router: Router, private notify: ToastrService, private imageCompress: NgxImageCompressService) {
 
-  onSubmit() {
-    this.service.save(this.model)
-      .subscribe((data: any) => {
-        this.notify.success(data.message, 'Success');
-        this.router.navigate(['/product/list']);
-      }, error => {
-        this.notify.error(error.error.message, 'Error');
-      });
-  }
-
-  fileLoad(event) {
-    this.removeImage();
-    const file = event.target.files[0];
-    if (file.type == "image/jpeg" || file.type == "image/png") {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        console.log(reader.result);
-        this.model.image=reader.result.toString();
-      };
-    } else {
-      console.log("Only images are allowed");
     }
-  }
 
-  removeImage(){
-    this.model.image=null;
-  }
+    ngOnInit(): void {
+    }
 
+    onSubmit() {
+        this.service.save(this.model)
+            .subscribe((data: any) => {
+                this.notify.success(data.message, 'Success');
+                this.router.navigate(['/product/list']);
+            }, error => {
+                this.notify.error(error.error.message, 'Error');
+            });
+    }
+
+    fileLoad(event) {
+        // this.removeImage();
+        const file = event.target.files[0];
+
+
+        if (file.type === 'image/jpeg' || file.type === 'image/png') {
+
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                let base64 = reader.result;
+                console.log(this.imageCompress.byteCount(base64));
+                const clearLoop = setInterval(() => {
+                    if (this.imageCompress.byteCount(base64) > 100000) {
+
+                        this.imageCompress.compressFile(base64.toString(), 1, 50, 50).then((result) => {
+                            base64 = result;
+                            console.log(this.imageCompress.byteCount(base64));
+                        });
+                    } else {
+                        this.model.image = base64.toString();
+                        clearInterval(clearLoop);
+                    }
+                }, 100);
+            };
+        } else {
+            this.notify.error('Only accepts image');
+        }
+    }
+
+    removeImage() {
+        this.model.image = null;
+    }
 }
