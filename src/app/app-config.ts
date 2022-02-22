6aad7362130d09351339b93eaff3ea23b11d533c
Rabin <rabin.dongol@btech.com.np>
2022-02-22 10:48:46 +0545

    added const for orderstatus and payment status

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,3 +1,5 @@
+import {SelectableValue} from './@core/class/selectable-value';
+
 export const MainUrl = 'https://sagun-baakas-admin.herokuapp.com/';
 
 export const configApiUrl = {
@@ -7,3 +9,14 @@ export const configApiUrl = {
     LOT_HISTORY_API: MainUrl + 'lotHistory',
     SALES_API: MainUrl + 'sales'
 };
+
+export const ORDER_STATUS: SelectableValue[] = [
+    {code: 0, title: 'Booked'},
+    {code: 1, title: 'Delivered'}
+];
+
+export const PAYMENT_STATUS: SelectableValue[] = [
+    {code: 0, title: 'Unpaid'},
+    {code: 1, title: 'Partial payment'},
+    {code: 2, title: 'Paid'}
+];
