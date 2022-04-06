737e515c392ca066edc3d070bd31422224b475c2
Rajhim Shahi <rajhimshahi@gmail.com>
2022-04-06 12:16:07 +0545

    working in dashboard

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import {ChartsModule} from 'ng2-charts';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
+    ChartsModule,
     DashboardRoutingModule
   ]
 })
